e959902b8d57006b94ebec4f98fc87854c24d615
LuuizPostal2 <142310416+LuuizPostal2@users.noreply.github.com>
2024-09-25 09:35:17 -0300

    Add files via upload

---
+++ projeto exercicio de java script/script.js
@@ -0,0 +1,12 @@
+const themeToggleBtn = document.getElementById('theme-toggle')
+const body = document.body;
+themeToggleBtn.addEventListener('click', function() {
+
+body.classList.toggle('dark-theme');
+
+if(body.classList.contains('dark-theme')) {
+    themeToggleBtn.textContent = 'Mudar para Tema Claro';
+} else {
+    themeToggleBtn.textContent = 'Mudar para Tema Escuro';
+}
+});
