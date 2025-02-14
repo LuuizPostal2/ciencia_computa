2959f2eab9909c5cb99c57c86d2b7111ab8edf20
LuuizPostal2 <142310416+LuuizPostal2@users.noreply.github.com>
2025-02-14 08:08:26 -0300

    Add files via upload

---
+++ luiz back-end/script.js
@@ -0,0 +1,13 @@
+let nome = "Ana";
+const idade = 16;
+var cidade = "São Paulo";
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Cidade: ", cidade);
+
+let texto = "Estou aprendendo JavaScript!";
+let numero = 2024;
+let verdadeiro = true;
+console.log(typeof texto, texto);
+console.log(typeof numero, numero);
+console.log(typeof verdadeiro, verdadeiro);
